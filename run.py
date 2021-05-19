3e5ff37f10429c31056127d63d3e155ce23f9265
edilson <edilson.w3g@gmail.com>
2021-05-19 11:46:45 -0300

    fix: adjusting running interval

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import constants
 import time
 import schedule
 import searchjob
 
-schedule.every(10).seconds.do(searchjob.job)
+schedule.every(constants.JOB_RUNNING_MINUTES_INTERVAL).minutes.do(searchjob.job)
 while 1:
     schedule.run_pending()
     time.sleep(1)
